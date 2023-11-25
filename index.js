9c43a96e9f7ace157ac68ff78e9deff695ca2fcc
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-25 12:06:42 +0600

    added for loop for copy an array

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 //There are 12 way to copy an array
-//1. Spread Operator:
-//===================
 
 const original = [1, 2, 3];
+//1. Spread Operator:
+//===================
 const copy = [...original];
 console.log(copy);
+
+//2. For loop:
+//=============
+const copy1 = [];
+for(let i = 0; i<original.length; i++){
+    copy1.push(original[i]);
+}
+console.log(copy1);
