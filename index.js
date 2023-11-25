e5f843e54c4d0bacf1d3bdd73d5fa231ca308398
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-25 12:10:41 +0600

    added while loop for copy an array

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ for(let i = 0; i<original.length; i++){
     copy1.push(original[i]);
 }
 console.log(copy1);
+
+
+//3. while loop
+//==================
+const copy2 = [];
+let i = 0;
+while(i<original.length) {
+    copy2.push(original[i]);
+    i++;
+}
+console.log(copy2);
