d2f1521c055bac9702908600ddd7abd1e11e897c
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-25 12:14:18 +0600

    added Array.from for copy an array

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ while(i<original.length) {
     i++;
 }
 console.log(copy2);
+
+
+//4. slice()
+//====================
+const copy3 = original.slice();
+console.log(copy3);
+
+
+//5. Array.from()
+//===================
+const copy4 = Array.from(original);
+console.log(copy4);
